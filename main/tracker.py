6f6630fcb4ce1ffdabd38ea4404d0c0634c13d6f
Alex <62477923+adanilkov@users.noreply.github.com>
2023-03-26 18:32:34 -0400

    proccess_args

--- main/tracker.py
+++ main/tracker.py
@@ -1,4 +1,48 @@
 from transaction import Transaction
+import sys
 
-def quit():
-  pass
+def print_usage():
+    print('''usage:
+      quit
+      show categories
+      add category
+      modify category
+      show transactions
+      add transaction
+      delete transaction
+      summarize transactions by date
+      summarize transactions by month
+      summarize transactions by year
+      summarize transactions by category
+      ''')
+    
+def process_args(arglist):
+    ''' examine args and make appropriate calls to TodoList'''
+    transactions = Transaction()
+    if arglist==[]:
+        print_usage()
+    elif arglist[0]=="show":
+        print_todos(todolist.selectActive())
+    elif arglist[0]=="showall":
+        print_todos(todos = todolist.selectAll())
+    elif arglist[0]=="showcomplete":
+        print_todos(todolist.selectCompleted())
+    elif arglist[0]=='add':
+        if len(arglist)!=3:
+            print_usage()
+        else:
+            todo = {'title':arglist[1],'desc':arglist[2],'completed':0}
+            todolist.add(todo)
+    elif arglist[0]=='complete':
+        if len(arglist)!= 2:
+            print_usage()
+        else:
+            todolist.setComplete(arglist[1])
+    elif arglist[0]=='delete':
+        if len(arglist)!= 2:
+            print_usage()
+        else:
+            todolist.delete(arglist[1])
+    else:
+        print(arglist,"is not implemented")
+        print_usage()
