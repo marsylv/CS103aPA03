a494592c6e9e0f9285c9e2155b7cf1d7de50f4ee
NathanSubra <50427172+NathanSubra@users.noreply.github.com>
2023-03-26 21:18:59 -0400

    test additions

--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -1,6 +1,11 @@
 from main.tracker import main
-from pytest import *
+import pytest
 
-def test_exit():
+def test_exit(monkeypatch, capsys):
+    user_input = "exit"
+    monkeypatch.setattr('builtins.input', lambda _: user_input)
+
     main()
-    
+
+    captured = capsys.readouterr()
+    assert captured.out == "Exiting the program now!"
